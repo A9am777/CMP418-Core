d7626cf03cea4dd1b590a45e93ebe923d388675b
Adam Hughes <ahughesformal@gmail.com>
2023-10-20 12:58:36 +0100

    Presumably dope sheet track baking has now been implemented

--- 2D/DopeSheet.cpp
+++ 2D/DopeSheet.cpp
@@ -7,6 +7,62 @@ namespace Animation
 
   }
 
+  DopeSheet2D::BakedTrack DopeSheet2D::bakeTrack(DetailedTrack& track) const
+  {
+    BakedTrack out;
+
+    // Bake the track for each attribute
+    for (Byte attributeID = 0; attributeID < AttributeType::AttributeCount; ++attributeID)
+    {
+      auto& subTrack = track.attributeTracks[attributeID];
+      if (subTrack.size())
+      {
+        // This track is in use, start build
+        out.subTracks.emplace_back();
+        auto& subProgress = out.subTracks.back();
+        
+        gef::Matrix33 transformProgress = gef::Matrix33::kIdentity;
+        float durationProgress = .0f;
+        for (auto& unrefinedKey : subTrack)
+        {
+          durationProgress += unrefinedKey.duration;
+
+          // Build and apply a transformation matrix for this key according to its type
+          switch (AttributeType attribType = static_cast<AttributeType>(attributeID))
+          {
+            case AttributeTranslation:
+            {
+              gef::Matrix33 translationMatrix = gef::Matrix33::kIdentity;
+              translationMatrix.SetTranslation(gef::Vector2(unrefinedKey.values[0], unrefinedKey.values[1]));
+              transformProgress = transformProgress * translationMatrix;
+            }
+            break;
+            case AttributeRotation:
+            {
+              gef::Matrix33 rotationMatrix = gef::Matrix33::kIdentity;
+              rotationMatrix.Rotate(unrefinedKey.values[0]);
+              transformProgress = transformProgress * rotationMatrix;
+            }
+            break;
+            default: throw; // TODO: implement
+          }
+
+          // Finally, build the key itself
+          subProgress.emplace_back();
+          {
+            auto& freshKey = subProgress.back();
+            freshKey.targetTransform = transformProgress;
+            freshKey.endTime = durationProgress;
+            freshKey.easeIn = unrefinedKey.easeIn;
+            freshKey.easeOut = unrefinedKey.easeOut;
+          }
+        }
+      }
+    }
+
+    return out;
+  }
+
   DopeSheet2D::DetailedTrack& DopeSheet2D::getTrack(Label name)
   {
     auto it = detailedSheet.trackNames.find(name);
@@ -20,7 +76,7 @@ namespace Animation
     // Be careful that this is temporary
     return detailedSheet.trackCollection[it->second];
   }
-  bool DopeSheet2D::doesTrackExist(Label name)
+  bool DopeSheet2D::doesTrackExist(Label name) const
   {
     auto it = detailedSheet.trackNames.find(name);
     return it != detailedSheet.trackNames.end();
--- 2D/DopeSheet.h
+++ 2D/DopeSheet.h
@@ -12,11 +12,11 @@ namespace Animation
   class DopeSheet2D
   {
     public:
-    enum InterpolationType : Byte
+    enum class InterpolationType : Byte
     {
       TweenLinear
     };
-    enum AttributeType : Byte // Global transform, translation, etc. Specifically the operation
+    enum AttributeType : Byte // Global transform, translation, etc. Specifically the operation. Ordered by transform application
     {
       AttributeFull = 0,
       AttributeTranslation,
@@ -30,7 +30,7 @@ namespace Animation
     };
     struct TweenPoint // Interpolation at a point
     {
-      InterpolationType tweenType = TweenLinear;
+      InterpolationType tweenType = InterpolationType::TweenLinear;
       float tweenWeight = .0f;
     };
     struct DetailedKeyframe
@@ -44,11 +44,29 @@ namespace Animation
     {
       std::array<std::list<DetailedKeyframe>, AttributeCount> attributeTracks;
     };
+    struct Keyframe
+    {
+      gef::Matrix33 targetTransform; // Precomputed
+      TweenPoint easeIn;
+      TweenPoint easeOut;
+      float endTime;
+    };
+    class BakedTrack
+    {
+      friend DopeSheet2D;
+      public:
+
+      private:
+      // Different attributes can have different interpolations therefore cannot be merged into one track and must be
+      // kept separate
+      std::vector<std::vector<Keyframe>> subTracks;
+    };
 
     DopeSheet2D();
 
+    BakedTrack bakeTrack(DetailedTrack& track) const; // Bakes a track to be ready for use
     DetailedTrack& getTrack(Label name); // Finds or creates a track of name
-    bool doesTrackExist(Label name);
+    bool doesTrackExist(Label name) const;
 
     inline void addTranslationKeyframe(DetailedTrack& track, float duration, const gef::Vector2& offset, const TweenPoint& in = TweenPoint(), const TweenPoint& out = TweenPoint())
     { addBaseKeyframe(track, duration, {offset.x, offset.y}, AttributeTranslation, in, out); }
@@ -65,11 +83,6 @@ namespace Animation
     {
       std::map<std::string, UInt> trackNames; // Name to allocation
       std::vector<DetailedTrack> trackCollection; // Raw per-object track
-    };
-    struct Keyframe
-    {
-      gef::Matrix33 targetTransform; // Precomputed
-      float endTime;
     };
 
     void addBaseKeyframe(DetailedTrack& track, float duration, const std::initializer_list<float>& params, AttributeType keyType, const TweenPoint& in, const TweenPoint& out);
