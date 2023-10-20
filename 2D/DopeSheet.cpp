34309d81b7fd59fdb526626a322cd75ebe921b1c
Adam Hughes <ahughesformal@gmail.com>
2023-10-20 10:56:01 +0100

    Adds ability to add tracks to the dopesheet

--- 2D/DopeSheet.cpp
+++ 2D/DopeSheet.cpp
@@ -1,5 +1,37 @@
+#include "DopeSheet.h"
 
 namespace Animation
 {
+  DopeSheet2D::DopeSheet2D() : sheetDuration{ .0f }, sheetRate{1.f}
+  {
 
+  }
+
+  DopeSheet2D::DetailedTrack& DopeSheet2D::getTrack(Label name)
+  {
+    auto it = detailedSheet.trackNames.find(name);
+    if (it == detailedSheet.trackNames.end())
+    {
+      // Create new
+      it = detailedSheet.trackNames.insert({ name,static_cast<UInt>(detailedSheet.trackCollection.size()) }).first;
+      detailedSheet.trackCollection.emplace_back();
+    }
+
+    // Be careful that this is temporary
+    return detailedSheet.trackCollection[it->second];
+  }
+  bool DopeSheet2D::doesTrackExist(Label name)
+  {
+    auto it = detailedSheet.trackNames.find(name);
+    return it != detailedSheet.trackNames.end();
+  }
+  void DopeSheet2D::addBaseKeyframe(DetailedTrack& track, float duration, const std::initializer_list<float>& params, AttributeType keyType, const TweenPoint& in, const TweenPoint& out)
+  {
+    track.attributeTracks[keyType].push_back({
+      params,
+      in,
+      out,
+      duration
+    });
+  }
 }
--- 2D/DopeSheet.h
+++ 2D/DopeSheet.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <array>
 #include <list>
+#include <map>
 #include "../Defs.h"
 
 namespace Animation
@@ -11,8 +12,6 @@ namespace Animation
   class DopeSheet2D
   {
     public:
-
-    private:
     enum InterpolationType : Byte
     {
       TweenLinear
@@ -31,32 +30,53 @@ namespace Animation
     };
     struct TweenPoint // Interpolation at a point
     {
-      InterpolationType tweenType;
-      float tweenWeight;
+      InterpolationType tweenType = TweenLinear;
+      float tweenWeight = .0f;
     };
     struct DetailedKeyframe
     {
       std::vector<float> values; // Pack of all values for the attribute type
       TweenPoint easeIn;
       TweenPoint easeOut;
+      float duration;
     };
     struct DetailedTrack
     {
-      std::string objectName;
       std::array<std::list<DetailedKeyframe>, AttributeCount> attributeTracks;
     };
+
+    DopeSheet2D();
+
+    DetailedTrack& getTrack(Label name); // Finds or creates a track of name
+    bool doesTrackExist(Label name);
+
+    inline void addTranslationKeyframe(DetailedTrack& track, float duration, const gef::Vector2& offset, const TweenPoint& in = TweenPoint(), const TweenPoint& out = TweenPoint())
+    { addBaseKeyframe(track, duration, {offset.x, offset.y}, AttributeTranslation, in, out); }
+    inline void addRotationKeyframe(DetailedTrack& track, float duration, float angle, const TweenPoint& in = TweenPoint(), const TweenPoint& out = TweenPoint())
+    { addBaseKeyframe(track, duration, { angle }, AttributeRotation, in, out); }
+    inline void addScaleKeyframe(DetailedTrack& track, float duration, const gef::Vector2& scale, const TweenPoint& in = TweenPoint(), const TweenPoint& out = TweenPoint())
+    { addBaseKeyframe(track, duration, { scale.x, scale.y}, AttributeScale, in, out); }
+
+    inline void setRate(float rate) { sheetRate = rate; }
+    inline void setDuration(float duration) { sheetDuration = duration; }
+
+    private:
     struct DetailedSheet
     {
-      std::vector<DetailedTrack> 
+      std::map<std::string, UInt> trackNames; // Name to allocation
+      std::vector<DetailedTrack> trackCollection; // Raw per-object track
     };
-
     struct Keyframe
     {
       gef::Matrix33 targetTransform; // Precomputed
       float endTime;
     };
 
-    float duration;
-    float framerate;
+    void addBaseKeyframe(DetailedTrack& track, float duration, const std::initializer_list<float>& params, AttributeType keyType, const TweenPoint& in, const TweenPoint& out);
+
+    DetailedSheet detailedSheet; // Sheet information prior to optimisation
+
+    float sheetDuration;
+    float sheetRate;
   };
 }
